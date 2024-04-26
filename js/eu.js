1285c518f53ae30b862e3e77c3fb71a90389ee76
gabriel <pinho.gabriel@edu.pucrs.br>
2024-04-26 14:02:49 -0300

    ajustes  finais

--- js/eu.js
+++
@@ -1,3 +0,0 @@
-"coord":{"lon":139.7595,"lat":35.6828},
-"weather":[{"id":500,"main":"Rain","description":"chuva leve","icon":"10d"},{"id":701,"main":"Mist","description":"névoa","icon":"50d"}],"base":"stations","main":{"temp":15.55,"feels_like":15.43,"temp_min":14.44,"temp_max":16.64,"pressure":1013,"humidity":87},"visibility":4000,"wind":{"speed":2.06,"deg":50},"rain":{"1h":0.15},"clouds":{"all":75},"dt":1713929836,"sys":{"type":2,"id":268395,"country":"JP","sunrise":1713902212,"sunset":1713950460},"timezone":32400,"id":1861060,"name":"Japan","cod":200}
-
